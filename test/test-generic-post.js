aa13d37e279f1f198df4487de8a074e8126d537c
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2020-09-24 23:29:39 -0300

    cambio algo de css

--- test/test-generic-post.js
+++ test/test-generic-post.js
@@ -55,12 +55,6 @@ describe("check build output for a generic post", () => {
       );
     });
 
-    it("should have inlined css", () => {
-      const css = select("style");
-      expect(css).to.match(/header nav/);
-      expect(css).to.not.match(/test-dead-code-elimination-sentinel/);
-    });
-
     it("should have script elements", () => {
       const scripts = doc.querySelectorAll("script[src]");
       expect(scripts).to.have.length(GA_ID ? 2 : 1);
